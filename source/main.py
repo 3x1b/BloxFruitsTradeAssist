98f2a43e4a72e943aa262f6d05e9eb5f3bc7dab8
3x1b <137458387+3x1b@users.noreply.github.com>
2023-12-30 18:46:56 -0500

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,228 +1,177 @@
-import tkinter as tk
-import tkinter.font as tkFont
+from customtkinter import *
 from pyautogui import alert
 
+app = CTk()
+app.geometry("550x350")
+app.title("TradeAssist 1.2.0")
+
+deactivate_automatic_dpi_awareness()
+set_appearance_mode("dark")
+
+app.resizable(False, False)
+
+
 fruits = {
-	"": 0,
-	"none": 0,
-	"kilo": 0,
-	"rocket": 0,
-	"spin": 0,
-	"chop": 0,
-	"spring": 0,
-	"bomb": 0,
-	"smoke": 0,
-	"spike": 0,
-	"flame": 0,
-	"falcon": 0,
-	"ice": 0,
-	"sand": 0,
-	"dark": 0,
-	"diamond": 0,
-	"rubber": 0.25,
-	"light": 0.5,
-	"barrier": 0.25,
-	"ghost": 0.25,
-	"magma": 1.5,
-	"quake": 3,
-	"buddha": 7,
-	"love": 4,
-	"spider": 4,
-	"sound": 5,
-	"phoenix": 4,
-	"portal": 6,
-	"rumble": 5,
-	"pain": 5,
-	"blizzard": 6,
-	"gravity": 4.5,
-	"mammoth": 8,
-	"trex": 7.5,
-	"dough": 19.5,
-	"shadow": 8,
-	"venom": 9.5,
-	"control": 9,
-	"spirit": 10.5,
-	"dragon": 40,
-	"leopard": 75,
-	"kitsune": 100,
-	"robux": 0.05
+    "": 0,
+    "none": 0,
+    "kilo": 0,
+    "rocket": 0,
+    "spin": 0,
+    "chop": 0,
+    "spring": 0,
+    "bomb": 0,
+    "smoke": 0,
+    "spike": 0,
+    "flame": 0,
+    "falcon": 0,
+    "ice": 0,
+    "sand": 0,
+    "dark": 0,
+    "diamond": 0,
+    "rubber": 0.25,
+    "light": 0.5,
+    "barrier": 0.25,
+    "ghost": 0.25,
+    "magma": 1.5,
+    "quake": 3,
+    "buddha": 7,
+    "love": 4,
+    "spider": 4,
+    "sound": 5,
+    "phoenix": 4,
+    "portal": 6,
+    "rumble": 5,
+    "pain": 5,
+    "blizzard": 6,
+    "gravity": 4.5,
+    "mammoth": 8,
+    "trex": 7.5,
+    "t-rex": 7.5,
+    "dough": 19.5,
+    "shadow": 8,
+    "venom": 9.5,
+    "control": 9,
+    "spirit": 10.5,
+    "dragon": 40,
+    "leopard": 75,
+    "kitsune": 100,
+    "robux": 0.05
 }
 
 hype = {
-	"buddha": 1.1,
-	"trex": 0.95,
-	"dough": 1.15,
-	"control": 1.05,
-	"dragon": 1.3,
-	"robux": 1.05
+    "buddha": 1.1,
+    "trex": 0.95,
+    "t-rex": 0.95,
+    "dough": 1.15,
+    "control": 1.05,
+    "dragon": 1.3,
+    "leopard": 1.15,
+    "kitsune": 1.25,
+    "robux": 1.05
+}
+
+gamepasses = {
+    
+}
+
+perm = {
+    
 }
 
 def calc(s1, s2, dm):
-	t1 = 0
-	for i in range(len(s1)):
-		if s1[i] in hype.keys() and dm == 1:
-			t1 += fruits[s1[i].lower()] * hype[s1[i].lower()]
-		else:
-			if s1[i].lower() not in fruits.keys():
-				return "error 1"
-			t1 += fruits[s1[i].lower()]
-	t2 = 0
-	for i in range(len(s2)):
-		if s2[i] in hype.keys() and dm == 1:
-			t2 += fruits[s2[i].lower()] * hype[s2[i].lower()]
-		else:
-			if s2[i].lower() not in fruits.keys():
-				return "error 1"
-			t2 += fruits[s2[i].lower()]
-	si = ""
-	if t1 == t2:
-		si = "="
-	elif t1 < t2:
-		si = "<"
-	elif t1 > t2:
-		si = ">"
-	return f"{str(s1)} {si} {str(s2)}\n{t1}|{t2}"
-
-class App:
-    def __init__(self, root):
-        #setting title
-        root.title("TradeAssist")
-        Checkbox_HypeAdjust_variable = tk.IntVar()
-        #setting window size
-        width=600
-        height=300
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
-
-        Label_Left=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=24)
-        Label_Left["font"] = tkFont.Font(family='Consolas',size=15)
-        Label_Left["fg"] = "#333333"
-        Label_Left["justify"] = "center"
-        Label_Left["text"] = "Left Side"
-        Label_Left.place(x=50,y=30,width=115,height=51)
-
-        Label_Right=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=22)
-        Label_Right["font"] = tkFont.Font(family='Consolas',size=15)
-        Label_Right["fg"] = "#333333"
-        Label_Right["justify"] = "center"
-        Label_Right["text"] = "Right Side"
-        Label_Right.place(x=400,y=30,width=115,height=51)
-
-        Textbox_1_1=tk.Entry(root)
-        Textbox_1_1["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_1_1["font"] = ft
-        Textbox_1_1["fg"] = "#333333"
-        Textbox_1_1["justify"] = "center"
-        Textbox_1_1["text"] = ""
-        Textbox_1_1.place(x=72,y=110,width=70,height=25)
-        self.Textbox_1_1 = Textbox_1_1
-
-        Textbox_1_2=tk.Entry(root)
-        Textbox_1_2["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_1_2["font"] = ft
-        Textbox_1_2["fg"] = "#333333"
-        Textbox_1_2["justify"] = "center"
-        Textbox_1_2["text"] = ""
-        Textbox_1_2.place(x=72,y=140,width=70,height=25)
-        self.Textbox_1_2 = Textbox_1_2
-
-        Textbox_1_3=tk.Entry(root)
-        Textbox_1_3["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_1_3["font"] = ft
-        Textbox_1_3["fg"] = "#333333"
-        Textbox_1_3["justify"] = "center"
-        Textbox_1_3["text"] = ""
-        Textbox_1_3.place(x=72,y=170,width=70,height=25)
-        self.Textbox_1_3 = Textbox_1_3
-
-        Textbox_1_4=tk.Entry(root)
-        Textbox_1_4["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_1_4["font"] = ft
-        Textbox_1_4["fg"] = "#333333"
-        Textbox_1_4["justify"] = "center"
-        Textbox_1_4["text"] = ""
-        Textbox_1_4.place(x=72,y=200,width=70,height=25)
-        self.Textbox_1_4 = Textbox_1_4
-
-        Textbox_2_1=tk.Entry(root)
-        Textbox_2_1["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_2_1["font"] = ft
-        Textbox_2_1["fg"] = "#333333"
-        Textbox_2_1["justify"] = "center"
-        Textbox_2_1["text"] = ""
-        Textbox_2_1.place(x=422,y=110,width=70,height=25)
-        self.Textbox_2_1 = Textbox_2_1
-
-        Textbox_2_2=tk.Entry(root)
-        Textbox_2_2["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_2_2["font"] = ft
-        Textbox_2_2["fg"] = "#333333"
-        Textbox_2_2["justify"] = "center"
-        Textbox_2_2["text"] = ""
-        Textbox_2_2.place(x=422,y=140,width=70,height=25)
-        self.Textbox_2_2 = Textbox_2_2
-
-        Textbox_2_3=tk.Entry(root)
-        Textbox_2_3["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_2_3["font"] = ft
-        Textbox_2_3["fg"] = "#333333"
-        Textbox_2_3["justify"] = "center"
-        Textbox_2_3["text"] = ""
-        Textbox_2_3.place(x=422,y=170,width=70,height=25)
-        self.Textbox_2_3 = Textbox_2_3
-
-        Textbox_2_4=tk.Entry(root)
-        Textbox_2_4["borderwidth"] = "1px"
-        ft = tkFont.Font(family='Consolas',size=10)
-        Textbox_2_4["font"] = ft
-        Textbox_2_4["fg"] = "#333333"
-        Textbox_2_4["justify"] = "center"
-        Textbox_2_4["text"] = ""
-        Textbox_2_4.place(x=422,y=200,width=70,height=25)
-        self.Textbox_2_4 = Textbox_2_4
-
-        Checkbox_HypeAdjust=tk.Checkbutton(root, variable=Checkbox_HypeAdjust_variable)
-        ft = tkFont.Font(family='Times',size=10)
-        Checkbox_HypeAdjust["font"] = tkFont.Font(family='Consolas',size=10)
-        Checkbox_HypeAdjust["fg"] = "#333333"
-        Checkbox_HypeAdjust["justify"] = "center"
-        Checkbox_HypeAdjust["text"] = "Adjust for hype?"
-        Checkbox_HypeAdjust.place(x=225,y=240,width=155,height=25)
-        Checkbox_HypeAdjust["offvalue"] = "0"
-        Checkbox_HypeAdjust["onvalue"] = "1"
-        Checkbox_HypeAdjust["command"] = self.Checkbox_HypeAdjust_command
-        self.Checkbox_HypeAdjust_variable = Checkbox_HypeAdjust_variable
-
-        Calculate_Button=tk.Button(root)
-        Calculate_Button["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Consolas',size=17)
-        Calculate_Button["font"] = tkFont.Font(family='Consolas',size=10)
-        Calculate_Button["fg"] = "#000000"
-        Calculate_Button["justify"] = "center"
-        Calculate_Button["text"] = "Calculate"
-        Calculate_Button.place(x=260,y=270,width=70,height=25)
-        Calculate_Button["command"] = self.Calculate_Button_command
-
-    def Checkbox_HypeAdjust_command(self):
-        pass
-
-    def Calculate_Button_command(self):
-        alert(calc([self.Textbox_1_1.get(), self.Textbox_1_2.get(), self.Textbox_1_3.get(), self.Textbox_1_4.get()], [self.Textbox_2_1.get(), self.Textbox_2_2.get(), self.Textbox_2_3.get(), self.Textbox_2_4.get()], self.Checkbox_HypeAdjust_variable.get()))
+    t1 = 0
+    for i in range(len(s1)):
+        if s1[i].lower() in hype.keys() and dm == 1:
+            t1 += fruits[s1[i].lower()] * hype[s1[i].lower()]
+        else:
+            if s1[i].lower() not in fruits.keys():
+                return "error 1"
+            t1 += fruits[s1[i].lower()]
+    t2 = 0
+    for i in range(len(s2)):
+        if s2[i].lower() in hype.keys() and dm == 1:
+            t2 += fruits[s2[i].lower()] * hype[s2[i].lower()]
+        else:
+            if s2[i].lower() not in fruits.keys():
+                return "error 1"
+            t2 += fruits[s2[i].lower()]
+    return [t1,t2]
 
 
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+left_frame = CTkFrame(master=app, width=150, height=200)
+left_frame.place(relx=0.2, rely=0.4, anchor="center")
+
+right_frame = CTkFrame(master=app, width=150, height=200)
+right_frame.place(relx=0.8, rely=0.4, anchor="center")
+
+entry_1_1 = CTkEntry(master=app)
+entry_1_1.place(relx=0.2, rely=0.17, anchor="center")
+
+entry_1_2 = CTkEntry(master=app)
+entry_1_2.place(relx=0.2, rely=0.32, anchor="center")
+
+entry_1_3 = CTkEntry(master=app)
+entry_1_3.place(relx=0.2, rely=0.47, anchor="center")
+
+entry_1_4 = CTkEntry(master=app)
+entry_1_4.place(relx=0.2, rely=0.62, anchor="center")
+
+entry_2_1 = CTkEntry(master=app)
+entry_2_1.place(relx=0.8, rely=0.17, anchor="center")
+
+entry_2_2 = CTkEntry(master=app)
+entry_2_2.place(relx=0.8, rely=0.32, anchor="center")
+
+entry_2_3 = CTkEntry(master=app)
+entry_2_3.place(relx=0.8, rely=0.47, anchor="center")
+
+entry_2_4 = CTkEntry(master=app)
+entry_2_4.place(relx=0.8, rely=0.62, anchor="center")
+
+left_value = CTkLabel(app, text="", fg_color="transparent", font=("Roboto Mono", 50))
+left_value.place(relx=0.2, rely=0.85, anchor="center")
+
+right_value = CTkLabel(app, text="", fg_color="transparent", font=("Roboto Mono", 50))
+right_value.place(relx=0.8, rely=0.85, anchor="center")
+
+def hype_switch_event():
+    pass
+
+hype_switch_var = IntVar(value=0)
+hype_switch = CTkSwitch(app, text="Adjust for hype?", command=hype_switch_event,
+    progress_color = "#2056e8",
+    variable=hype_switch_var, onvalue=1, offvalue=0)
+hype_switch.place(relx=0.5, rely=0.8, anchor="center")
+
+def calculate_button_event():
+    vals = calc([entry_1_1.get(), entry_1_2.get(), entry_1_3.get(), entry_1_4.get()], 
+                [entry_2_1.get(), entry_2_2.get(), entry_2_3.get(), entry_2_4.get()], 
+                hype_switch_var.get())
+    if vals == "error 1":
+        alert("Error 1: Invalid Fruit!")
+    else:
+        rvals = []
+        rvals.append(round(vals[0]*10)/10)
+        rvals.append(round(vals[1]*10)/10)
+
+        cols = ["", ""]
+
+        if rvals[0] == rvals[1]:
+            cols = ["yellow", "yellow"]
+        elif rvals[0] > rvals[1]:
+            cols = ["green", "red"]
+        elif rvals[0] < rvals[1]:
+            cols = ["red", "green"]
+        else:
+            cols = ["red", "red"]
+
+        left_value.configure(text=str(rvals[0]), text_color=cols[0])
+        right_value.configure(text=str(rvals[1]), text_color=cols[1])
+
+
+calculate_button = CTkButton(master=app, text="Calculate!", command=calculate_button_event, corner_radius=6, border_width=2,
+    fg_color="#2056e8", hover_color="#1630b6", border_color="#113085")
+calculate_button.place(relx=0.5, rely=0.9, anchor="center")
+
+
+app.mainloop()
